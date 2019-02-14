95a077d673b3e7a9b711c08f984b3750f341c61d
Artyom <taranenko@grechka.digital>
2019-02-14 18:00:05 +0300

    task1

---
+++ script.js
@@ -0,0 +1,14 @@
+// function contrastImg() {
+// 	var contrast = document.getElementById('a1').value;
+// 	console.log(contrast);
+// 	var img = document.getlementsByClassName('js-img');
+// 	return contrast;
+// 	// img.style.width = contrast + 'px';
+// }
+
+// function lighteenImg() {
+
+// }
+// function zoomImg() {
+
+// }
