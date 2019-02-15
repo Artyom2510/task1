bab77a5164ea52af5bfe9b16336bf2fbecadb351
Artyom <taranenko@grechka.digital>
2019-02-15 17:57:16 +0300

    need end

--- script.js
+++ script.js
@@ -1,46 +1,101 @@
 window.onload = function() {
 
-	var img = document.getElementById('img');
+	var img = document.querySelector('.js-img');
+	var zoom = document.querySelector('.js-zoom').value;
+	var pos = {
+		top: 0,
+		left: 0
+	};
 
-	document.querySelector('.js-invert').oninput = function() {
+	document.querySelector('.js-contrast').oninput = function() {
 		function contrastImg() {
-			var invert = document.querySelector('.js-invert').value;
-			img.style.filter = "invert(" + invert + "%)";
+			var contrast = document.querySelector('.js-contrast').value;
+			var brightness = document.querySelector('.js-brightness').value;
+			img.style.filter = "brightness(" + brightness + "%)" + " contrast(" + contrast + "%)";
 		}
 		contrastImg();
 	}
 	
 	document.querySelector('.js-brightness').oninput = function() {
 		function brightnesstImg() {
+			var contrast = document.querySelector('.js-contrast').value;
 			var brightness = document.querySelector('.js-brightness').value;
-			img.style.filter = "brightness(" + brightness + "%)";
+			img.style.filter = "brightness(" + brightness + "%)" + " contrast(" + contrast + "%)";
 		}
 		brightnesstImg();
 	}
 	
 	document.querySelector('.js-zoom').oninput = function() {
 		function zoomImg() {
-			var zoom = document.querySelector('.js-zoom').value;
-			img.style.width = zoom + "%";
+			zoom = document.querySelector('.js-zoom').value;
+			img.style.width = +zoom + 50 + "%";
+			img.style.height = +zoom + 50 + "%";
+			if(zoom > 50) {
+				update();
+			}
+			// if(zoom > 50) {
+			// 	heightImg = img.offsetHeight;
+			// 	freePlaceTop = heightBlock - heightImg;
+			// 	console.log(freePlaceTop);
+			// }
 		}
 		zoomImg();
 	}
-	var top = bottom = left = right = 0;
+
+	// function move() {
+	// 	// var zoom = document.querySelector('.js-zoom').value;
+	// 	console.log(zoom);
+	// 	if(zoom > 50) {
+	// 		function update() {
+	// 			img.style.top = pos.top + "%";
+	// 			img.style.left = pos.left + "%";
+	// 		}
+	// 		document.querySelector('.js-top').onclick = function() {
+	// 			pos.top -= 10;
+	// 			update();
+	// 		}
+	// 		document.querySelector('.js-bottom').onclick = function() {
+	// 			pos.top += 10;
+	// 			update();
+	// 		}
+	// 		document.querySelector('.js-left').onclick = function() {
+	// 			pos.left += 10;
+	// 			update();
+	// 		}
+	// 		document.querySelector('.js-right').onclick = function() {
+	// 			pos.left -= 10;
+	// 			update();
+	// 		}
+	// 		document.querySelector('.js-middle').onclick = function() {
+	// 			pos.left = 0;
+	// 			pos.top = 0;
+	// 			update();
+	// 		}
+	// 	}
+	// }
+
+	function update() {
+		img.style.top = pos.top + "%";
+		img.style.left = pos.left + "%";
+	}
 	document.querySelector('.js-top').onclick = function() {
-		var thisBtn = img.style.top = -10 * top++ + "%"
-		// img.style += '-o-transform: translateY('+  10 * top++ + "%" +');'; 
-		console.log(thisBtn);
-		if (top == 11) {
-			top = 0;
-		}
-		return top;
+		pos.top -= 10;
 	}
 	document.querySelector('.js-bottom').onclick = function() {
-		var thisBtn1 = img.style.bottom =  -10 * bottom++ + "%";
-		console.log(thisBtn1);
-		if(bottom == 11) {
-			bottom = 0;
-		}
+		pos.top += 10;
+		update();
 	}
-
+	document.querySelector('.js-left').onclick = function() {
+		pos.left += 10;
+		update();
+	}
+	document.querySelector('.js-right').onclick = function() {
+		pos.left -= 10;
+		update();
+	}
+	document.querySelector('.js-middle').onclick = function() {
+		pos.left = 0;
+		pos.top = 0;
+		update();
+	}
 }
