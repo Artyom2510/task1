4845ffce319298080ef1fe36241ca65d7840d0b6
Artyom <taranenko@grechka.digital>
2019-02-19 17:46:00 +0300

    need lupa-zalupa

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.onload = function() {
 
 	var img = document.querySelector('.js-img');
+	var btn = document.querySelectorAll('.btn');
+	var right = document.querySelector('.buttons__right');
+	var left = document.querySelector('.buttons__left');
+	var top = document.querySelector('.buttons__top');
+	var bottom = document.querySelector('.buttons__bottom');
+	var middle = document.querySelector('.buttons__middle');
 	var state = {
 		contrast: 50,
 		brightness: 50,
@@ -21,25 +27,48 @@ window.onload = function() {
 		brightness = document.querySelector('.js-brightness').value;
 		img.style.filter = "brightness(" + brightness + "%)" + " contrast(" + contrast + "%)";
 		state.contrast = +contrast;
+		if( brightness == 50 && contrast == 50 && state.zoom == 100 ) {
+			middle.classList.add('btn_stop');
+		} else {
+			middle.classList.remove('btn_stop');
+		}
 	}
 	function brightnessImg() {
 		contrast = document.querySelector('.js-contrast').value;
 		brightness = document.querySelector('.js-brightness').value;
 		img.style.filter = "brightness(" + brightness + "%)" + " contrast(" + contrast + "%)";
 		state.brightness = +brightness;
+		if( brightness == 50 && contrast == 50 && state.zoom == 100 ) {
+			middle.classList.add('btn_stop');
+		} else {
+			middle.classList.remove('btn_stop');
+		}
 	}
-
 	function zoomImg() {
 		zoom = document.querySelector('.js-zoom').value;
 		state.zoom = +zoom;
 		img.style.width = +zoom + "%";
 		img.style.height = +zoom + "%";
+		middle.classList.remove('btn_stop');
 		if( zoom == 100 ) {
 			state.left = 0;
 			state.top = 0;
-			document.querySelectorAll('.btn').forEach(e => e.setAttribute('disabled', ''));
-		} else {
-			document.querySelectorAll('.btn').forEach(e => e.removeAttribute('disabled'));
+			bottom.classList.add('btn_stop');
+			top.classList.add('btn_stop');
+			left.classList.add('btn_stop');
+			right.classList.add('btn_stop');
+			// btn.filter(b => b.classList.contains('middle')).forEach( e => {
+			// 	if( !e.classList.contains('btn_stop')) {
+			// 		e.classList.add('btn_stop');
+			// 	}
+			// });
+			if( state.brightness == 50 && state.contrast == 50 ) {
+				middle.classList.add('btn_stop');
+			}
+			// btn.forEach(e => e.classList.add('btn_stop'));
+		 } else {
+			top.classList.remove('btn_stop');
+			right.classList.remove('btn_stop');
 		}
 	}
 
@@ -58,37 +87,68 @@ window.onload = function() {
 		update();
 	});
 
-	document.querySelector('.js-bottom').addEventListener('click', function() {
-		img.scrollLeft += 10;
-		update();
+	bottom.addEventListener('click', function() {
+		if( state.top < 0 ) {
+			state.top += 10;
+			top.classList.remove('btn_stop');
+			update();
+		}
+		if( state.top == 0 ) {
+			bottom.classList.add('btn_stop');
+		}
 	});
-	document.querySelector('.js-top').addEventListener('click', function() {
-		state.top -= 10;
-		update();
+	top.addEventListener('click', function() {
+		if( state.top + state.zoom - 100 > 0 ) {
+			state.top -= 10;
+			bottom.classList.remove('btn_stop');
+			update();
+		}
+		if( state.top + state.zoom - 100 == 0 ) {
+			top.classList.add('btn_stop');
+		}
 	});
-	document.querySelector('.js-left').addEventListener('click', function() {
-		state.left += 10;
-		update();
+	left.addEventListener('click', function() {
+		if( state.left < 0 ) {
+			state.left += 10;
+			right.classList.remove('btn_stop');
+			update();
+		}
+		if( state.left == 0 ) {
+			left.classList.add('btn_stop');
+		}
 	});
 	document.querySelector('.js-right').addEventListener('click', function() {
-		state.left -= 10;
-		update();
+		if( state.left + state.zoom - 100 > 0 ) {
+			state.left -= 10;
+			left.classList.remove('btn_stop');
+			update();
+		}
+		if( state.left + state.zoom - 100 == 0 ) {
+			right.classList.add('btn_stop');
+		}	
 	});
 
 
-	document.querySelector('.js-middle').addEventListener('click', function() {
-		state.left = 0;
-		state.top = 0;
-		state.brightness = 50;
-		state.contrast = 50;
-		state.zoom = 100;
-		document.querySelector('.js-zoom').value = 100;
-		document.querySelector('.js-contrast').value = 50;
-		document.querySelector('.js-brightness').value = 50;
-		contrastImg();
-		brightnessImg();
-		zoomImg();
-		update();
+	middle.addEventListener('click', function() {
+		if( state.zoom != 100 || state.brightness != 50 || state.contrast != 50 ) {
+			state.left = 0;
+			state.top = 0;
+			state.brightness = 50;
+			state.contrast = 50;
+			state.zoom = 100;
+			document.querySelector('.js-zoom').value = 100;
+			document.querySelector('.js-contrast').value = 50;
+			document.querySelector('.js-brightness').value = 50;
+			btn.forEach(e => {
+				if( !e.classList.contains('btn_stop') ) {
+					e.classList.add('btn_stop');
+				}
+			});
+			contrastImg();
+			brightnessImg();
+			zoomImg();
+			update();
+		}
 	});
 
 }
